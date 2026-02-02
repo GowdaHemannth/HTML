a88c66f8c59ebd6fea61ce5dc4f1babdb5406e1f
GowdaHemannth <hemanthagalahalli@gmail.com>
2026-02-02 19:30:12 +0530

    New Web design

--- JavaScript/Operator.js
+++ JavaScript/Operator.js
@@ -1,71 +1,180 @@
-// // console.log(5 & 2);
-// // console.log(9 & 3);
+// // // // // // // // console.log(5 & 2);
+// // // // // // // // console.log(9 & 3);
 
-// // let Data = null ?? "Optimal_Solution";
-// // console.log(Data);
-// // L = 3;
-// // if (L == 2) {
-// //   console.log("Dont trouble the Troble");
-// // } else if (L == 1) {
-// //   console.log("if trouble Troubles You");
-// // } else {
-// //   console.log("I am Not the Trouble i am The Truth");
-// // }
+// // // // // // // // let Data = null ?? "Optimal_Solution";
+// // // // // // // // console.log(Data);
+// // // // // // // // L = 3;
+// // // // // // // // if (L == 2) {
+// // // // // // // //   console.log("Dont trouble the Troble");
+// // // // // // // // } else if (L == 1) {
+// // // // // // // //   console.log("if trouble Troubles You");
+// // // // // // // // } else {
+// // // // // // // //   console.log("I am Not the Trouble i am The Truth");
+// // // // // // // // }
 
-// // var Age = 45;
-// // if (Age>=18 && Age<=70) {
-// //   console.log("Entry Allowed");
-// //   if (Age>=40 && Age<=60) {
-// //     console.log("Only Soft Drinks Allowed");
-// //   }
+// // // // // // // // var Age = 45;
+// // // // // // // // if (Age>=18 && Age<=70) {
+// // // // // // // //   console.log("Entry Allowed");
+// // // // // // // //   if (Age>=40 && Age<=60) {
+// // // // // // // //     console.log("Only Soft Drinks Allowed");
+// // // // // // // //   }
 
-// // }
+// // // // // // // // }
 
-// // if (Age < 18) {
-// //   console.log("No Entry");
-// // }
+// // // // // // // // if (Age < 18) {
+// // // // // // // //   console.log("No Entry");
+// // // // // // // // }
 
-// let Day = 1;
-// switch (Day) {
-//   case 1:
-//     console.log("Dont Trouble The trouble");
-//   case 2:
-//     console.log("If You Trouble the Trouble");
-//   case 3:
-//     console.log("Trouble Troubles You");
-//   case 4:
-//     console.log("I am Not The Trouble ");
-//   case 6:
-//     console.log("whdden sisiisis");
+// // // // // // let Day = 5;
+// // // // // // switch (Day) {
+// // // // // //   case 1+0:
+// // // // // //     console.log("Dont Trouble The trouble");
+// // // // // //   case 2+3:
+// // // // // //     console.log("If You Trouble the Trouble");
+// // // // // //     break;
+// // // // // //   case 2+5:
+// // // // // //     console.log("Trouble Troubles You");
+// // // // // //   case 4:
+// // // // // //     console.log("I am Not The Trouble ");
+// // // // // //   case 6:
+// // // // // //     console.log("whdden sisiisis");
+// // // // // // }
+// // // // // // // // Eligibility if it is Marks >35 and attendence >80
+// // // // // // // Grade section 90 to 100 a+ 75 89 A 60 74 B 35 59 c
+// // // // // // // Third Section ScolorShip Only if Grade A+ OR A  Famil inCome Should be Less than 200000 then Fully Internship
+// // // // // // // Ebven tgough he has A+ a if Family Income less than 400000 half Scolorship
+// // // // // // //
+// // // // // // let Income = 10000000;
+// // // // // // let Marks = 70;
+// // // // // // let Attendence = 98;
+// // // // // // if (Marks >= 35 && Attendence >= 80) {
+// // // // // //   if (Marks >= 90 || (Marks < 90 && Marks > 74)) {
+// // // // // //     if (Marks >= 90) {
+// // // // // //       console.log("Print HE GOT A+");
+// // // // // //     }
+// // // // // //     if (Marks < 90 && Marks > 74) {
+// // // // // //       console.log("He got A");
+// // // // // //     }
+// // // // // //     if (Income <= 200000) {
+// // // // // //       console.log("HE gOt Scolorship");
+// // // // // //     } else if (Income > 200000 && Income < 400000) {
+// // // // // //       console.log("He Will get Half Inership");
+// // // // // //     } else if (Income > 400000) {
+// // // // // //       console.log("No Scoloshipo");
+// // // // // //     }
+// // // // // //   } else if (Marks < 74 && Marks > 35) {
+// // // // // //     console.log("He will Get B");
+// // // // // //   } else {
+// // // // // //     console.log("He Will get C");
+// // // // // //   }
+// // // // // // }
+// // // // // // else {
+// // // // // //   console.log("He Is Fail");
+// // // // // // }
+
+// // // // // // let a=150
+// // // // // // let b=100
+// // // // // // a>b?console.log("Greater is a"):console.log("B is greater ");
+
+// // // // // let a=200;
+// // // // // let b=250;
+// // // // // let c=300
+// // // // // a>b?(a>c)?console.log("AA is Greater"):console.log("c is Greater"):b>c?console.log("b is G"):console.log("c");
+
+// // // // // let i=0
+// // // // // for(console.log("hi");i<10;console.log("bii"))
+// // // // // {
+// // // // //   console.log("Hai");
+// // // // //   i++
+
+// // // // // }
+// // // // // // Check even and odd using Ternary Operator
+// // // // // // Positive or Negative by Ternary
+// // // // // // greater of 4 Number
+// // // // // // Sammlest OF tHREE nUMBER
+// // // // // // Truthy Or Falsey
+
+// // // // // Assignment
+// // // // let Num=10
+// // // // Num%2==0?console.log("The Number is Even"):console.log("The Given Number is Odd");
+
+// // // // // 2nd Question
+// // // // let Num1=2
+// // // // Num1>=0?console.log("The Given Is Number is Positive"):console.log("The Give number is Negative");
+
+// // // // //3rd Question
+// // // // // Greatest of 4 Number
+
+// // // // let a = 10
+// // // // let b = 50
+// // // // let c = 20
+// // // // let d = 40
+
+// // // // a > b ? (a > c ? (a > d ? console.log("A is greater") : console.log("D is greater")) : (c > d ? console.log("C is greater") : console.log("D is greater"))) : (b > c ? (b > d ? console.log("B is greater") : console.log("D is greater")) : (c > d ? console.log("C is greater") : console.log("D is greater")));
+
+// // // // // Smallst of Three Number
+// // // // let A=10
+// // // // let B=20
+// // // // let C=30
+// // // // A<B?(A<C)?console.log("AA is Smallest"):console.log("c is Smallest"):B<C?console.log("b is Samllest"):console.log("c is Smmalest");
+
+// // // // //5th Numerical
+
+// // // // let S=1
+
+// // // // // console.log(Boolean(S));
+
+// // // // // console.log(S && Boolean(S));
+
+// // // // S&& Boolean() ?console.log("The Mentined is true "):console.log("Mentiond is False");
+
+// // // for (let i = 5; i <= 10; i++) {
+// // //   for (let j = 1; j <= 10; j++) {
+// // //     console.log(i + "*" + j + "=" + j * i);
+// // //   }
+// // // }
+
+// // // let i=0;
+// // // while(i<=10){
+// // //   console.log("O Captain My Captain");
+// // //   i++
+  
+// // // }
+
+// // // DO WHILE LOOP
+// // let i=1
+
+// // do{
+// //   let j=1
+// //  do{
+// //   console.log(i*j);
+// //   j++
+// //  }while(j<=10)
+ 
+// //   i++
+// // }while(i<=3)
+
+// for(let A=10;A>=1;A--){
+//   for(let B=10;B>=1;B--){
+//     console.log(A + "*" + B + "=" + B * A);
+    
+//   } 
+//   console.log(" ");
+  
 // }
-// // Eligibility if it is Marks >35 and attendence >80
-// Grade section 90 to 100 a+ 75 89 A 60 74 B 35 59 c
-// Third Section ScolorShip Only if Grade A+ OR A  Famil inCome Should be Less than 200000 then Fully Internship
-// Ebven tgough he has A+ a if Family Income less than 400000 half Scolorship
-//
-let Income = 10000000;
-let Marks = 70;
-let Attendence = 98;
-if (Marks >= 35 && Attendence >= 80) {
-  if (Marks >= 90 || (Marks < 90 && Marks > 74)) {
-    if (Marks >= 90) {
-      console.log("Print HE GOT A+");
-    }
-    if (Marks < 90 && Marks > 74) {
-      console.log("He got A");
-    }
-    if (Income <= 200000) {
-      console.log("HE gOt Scolorship");
-    } else if (Income > 200000 && Income < 400000) {
-      console.log("He Will get Half Inership");
-    } else if (Income > 400000) {
-      console.log("No Scoloshipo");
-    }
-  } else if (Marks < 74 && Marks > 35) {
-    console.log("He will Get B");
-  } else {
-    console.log("He Will get C");
-  }
-} else {
-  console.log("He Is Fail");
+
+
+function Call(Name="IronMan"){
+  console.log(Name,"Spideyy");
+  
 }
+
+Call("Capatain-America")
+
+function Cal(a,b,e){
+  return a+b+e
+}
+console.log(Cal(5,6,7));
+let v=Cal(8,9,10)
+console.log(v);
+
